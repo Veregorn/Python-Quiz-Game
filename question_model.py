f64ac15c29981438a146865d4c236d736737dbbf
Veregorn <rjbarco@gmail.com>
2023-11-07 15:07:41 +0100

    Question class created

---
+++ question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    
+    # Initializer
+    def __init__(self, text, answer) -> None:
+        self.text = text
+        self.answer = answer
---
+++ quiz_brain.py
@@ -0,0 +1,7 @@
+class QuizBrain:
+    def __init__(self, question_list) -> None:
+        self.question_number = 0
+        self.question_list = question_list
+
+    def next_question():
+        pass
